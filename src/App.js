38faa084807938aa366810aa86c077147e1119ff
TheMagicNacho <justinhasanaccount@gmail.com>
2022-02-16 12:56:59 -0500

    good enough

--- src/App.js
+++ src/App.js
@@ -19,16 +19,26 @@ const urlList = `http://52.26.193.201:3000/products/list`;
 function App() {
   const [productData, setProductData] = React.useState([]);
   const [viewingState, setViewingState] = React.useState({});
-  const [itemImgSrc, setItemImgSrc] = React.useState('');
+  const [itemData, setItemData] = React.useState('');
 
 // get image url
   React.useEffect (()=>{
     let urlItem = `http://52.26.193.201:3000/products/${viewingState.id}/styles`
     async function fetchItem() {
+      let itemObj = {};
       const response = await fetch(urlItem);
       const result = await response.json();
       // console.log(result.results[0].photos[0].url);
-      setItemImgSrc(result.results[0].photos[0].url);
+      let currentIndex = viewingState.index;
+
+      itemObj.imgUrl = result.results[0].photos[0].url;
+      itemObj.name = productData[currentIndex].name;
+      itemObj.slogan = productData[currentIndex].slogan;
+      itemObj.description = productData[currentIndex].description;
+      itemObj.category = productData[currentIndex].category;
+      itemObj.default_price = productData[currentIndex].default_price;
+      
+      setItemData(itemObj)
     }
     if(viewingState.userEvaluating){
       fetchItem()
@@ -36,6 +46,7 @@ function App() {
     
   });
 
+ console.log(itemData);
 
   React.useEffect(() => {
     async function fetchProducts() {
@@ -50,10 +61,12 @@ function App() {
   function handleLearnMore(key){
     let viewObject = {
       id: null,
+      index: null,
       userEvaluating: false
     }
     viewObject.userEvaluating = (viewingState.userEvaluating && key === viewingState.id) ? false : true;
     viewObject.id = key;
+    viewObject.index = key - 1;
     setViewingState(viewObject);
   }
 
@@ -65,11 +78,39 @@ function App() {
         {/* Browser Window */}
         <Grid container>
           {/* Top Bar */}
-          <Grid xs={12}>
+          <Grid xs={8}>
             <Card>
-              <CardContent>🧑‍🚀</CardContent>
+
+              <CardContent>
+               
+              <Typography variant="h6" component="div">
+                      {itemData.name}
+              </Typography>
+              <Typography variant="subtitle1" component="div">
+                      {itemData.slogan}
+              </Typography>
+              <Typography variant="body1" component="div">
+                      {itemData.description}
+              </Typography>
+              </CardContent>
+      
             </Card>
           </Grid>
+          <Grid xs={4}>
+            <Card>
+
+              <CardContent>
+               
+                <img width='100' src={itemData.imgUrl} alt={itemData.name} />
+              <Typography variant="subtitle1" component="div">
+              Ƀ{itemData.default_price}
+              </Typography>
+
+              </CardContent>
+      
+            </Card>
+          </Grid>
+
 
           {/* Body */}
           <Grid container xs={12}>
